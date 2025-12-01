ac418df68dcd64983dcd2335fcbd80f35f97fe77
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-01 13:16:52 -0600

    fix: reemplazar componente Link por un enlace HTML en la página de detalles del tema PARA VERCEL

--- src/pages/temas/[slug].tsx
+++ src/pages/temas/[slug].tsx
@@ -1,7 +1,6 @@
 import type { PageProps } from 'waku/router';
 import { getTemaBySlug } from '../../lib/utils';
 import MarkdownRenderer from '../../components/MarkdownRenderer.server';
-import { Link } from 'waku/router/client';
 
 export default async function PostDetail({ slug }: PageProps<'/temas/[slug]'>) {
   const tema = await getTemaBySlug(slug);
@@ -16,7 +15,7 @@ export default async function PostDetail({ slug }: PageProps<'/temas/[slug]'>) {
           <h1>Tema no encontrado</h1>
           <p>El tema <code>{slug}</code> no existe.</p>
           <p>
-            <Link to="/" className="text-blue-600 underline">Volver al inicio</Link>
+            <a href="/" className="text-blue-600 underline">Volver al inicio</a>
           </p>
         </div>
       </>
