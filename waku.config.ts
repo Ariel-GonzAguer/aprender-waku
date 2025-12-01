4c45d404822865390108fab90be42ba115da37a7
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-01 13:09:39 -0600

    fix: actualizar comando de construcción a "NETLIFY=1 waku build" y eliminar adapter de waku.config

--- waku.config.ts
+++ waku.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "waku/config";
 
 export default defineConfig({
-  adapter: "netlify",
   vite: {
     plugins: [
       // @ts-ignore
