d87801b7b70e0d0b8e1ac11c61cca5470eb09951
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-01 13:05:43 -0600

    fix: AGREGAR adapter: "netlify" a waku.config

--- waku.config.ts
+++ waku.config.ts
@@ -1,14 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'waku/config';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "waku/config";
 
 export default defineConfig({
+  adapter: "netlify",
   vite: {
     plugins: [
+      // @ts-ignore
       tailwindcss(),
+      // @ts-ignore
       react({
         babel: {
-          plugins: ['babel-plugin-react-compiler'],
+          plugins: ["babel-plugin-react-compiler"],
         },
       }),
     ],
