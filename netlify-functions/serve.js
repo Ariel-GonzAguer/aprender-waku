e96520f26973205684dbea712956fc737963e847
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-01 13:37:23 -0600

    PARA VERCEL fix: eliminar configuración de Netlify y ajustar script de construcción en package.json

--- netlify-functions/serve.js
+++
@@ -1,9 +0,0 @@
-const { INTERNAL_runFetch } = await import('../dist/server/index.js');
-
-export default async (request, context) =>
-  INTERNAL_runFetch(process.env, request, { context });
-
-export const config = {
-  preferStatic: true,
-  path: ['/', '/*'],
-};
