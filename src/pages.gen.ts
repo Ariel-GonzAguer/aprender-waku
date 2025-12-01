63b1523e07c48180c2f33e796bdc7d0108523fc9
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-01 11:48:23 -0600

    feat: agregar página dinámica para ejemplos con manejo de errores y enlaces de navegación

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -6,8 +6,6 @@ import type { PathsForPages, GetConfigResponse } from 'waku/router';
 // prettier-ignore
 import type { getConfig as File_About_getConfig } from './pages/about';
 // prettier-ignore
-import type { getConfig as File_EjemplosCatchallCatchAll_getConfig } from './pages/ejemplos/catchall/[...catchAll]';
-// prettier-ignore
 import type { getConfig as File_EjemplosSlug_getConfig } from './pages/ejemplos/[slug]';
 // prettier-ignore
 import type { getConfig as File_GatosSlugIndex_getConfig } from './pages/gatos/[slug]/index';
@@ -23,7 +21,6 @@ import type { getConfig as File_Root_getConfig } from './pages/_root';
 // prettier-ignore
 type Page =
 | ({ path: '/about' } & GetConfigResponse<typeof File_About_getConfig>)
-| ({ path: '/ejemplos/catchall/[...catchAll]' } & GetConfigResponse<typeof File_EjemplosCatchallCatchAll_getConfig>)
 | ({ path: '/ejemplos/[slug]' } & GetConfigResponse<typeof File_EjemplosSlug_getConfig>)
 | ({ path: '/gatos/[slug]' } & GetConfigResponse<typeof File_GatosSlugIndex_getConfig>)
 | ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>)
