3201acc31b8d555947dfc2d0c4fc9f643c23dd1b
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-12-07 17:36:45 -0600

    feat: eliminar componentes ClientBoundary y ClientComponenteConPrivate, actualizar página para reflejar cambios

--- src/pages/pizzas/paginaConTextoPrivado.tsx
+++ src/pages/pizzas/paginaConTextoPrivado.tsx
@@ -1,6 +1,4 @@
 import ServerComponentConPrivate from "../../components/ejemplos/ServerComponenteConPrivate";
-import ClientComponenteConPrivate from "../../components/ejemplos/ClientComponenteConPrivate";
-import ClientBoundary from "../../components/ClientBoundary";
 
 export default function PaginaConImagenPrivada() {
 
@@ -8,11 +6,6 @@ export default function PaginaConImagenPrivada() {
     <section className="flex flex-col justify-center items-center text-center mb-10">
       <p className="my-6">Server Component que usa imagen de `./private`</p>
       <ServerComponentConPrivate />
-      <p className="my-6 text-amber-300 font-extrabold text-3xl">- - - - - - </p>
-      <p className="my-6">Client Component que usa imagen de `./private`</p>
-      <ClientBoundary texto="Error capturado con react-error-boundary en Client Component">
-        <ClientComponenteConPrivate />
-      </ClientBoundary>
     </section>
   )
 }
